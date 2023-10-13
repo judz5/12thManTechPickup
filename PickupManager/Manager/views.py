b6c0e7d1baa93a4bcaa6816c1ad2f3f2f7bc06c1
judz5 <judz1105@gmail.com>
2023-10-13 16:29:59 -0500

    init basic homepage

--- PickupManager/Manager/views.py
+++ PickupManager/Manager/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
