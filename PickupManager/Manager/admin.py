50832adf6757f29e70fd6c8cba59b2a6d4c20677
judz5 <judz1105@gmail.com>
2023-10-13 16:55:32 -0500

    created base models

--- PickupManager/Manager/admin.py
+++ PickupManager/Manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cubby, OrdType, Order
 
 # Register your models here.
+
+admin.site.register(Cubby)
+admin.site.register(OrdType)
+admin.site.register(Order)
--- PickupManager/Manager/models.py
+++ PickupManager/Manager/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Cubby(models.Model):
+    location = models.CharField(max_length=2)
+    # also some way to track all orders assosciated with
+
+    def __str__(self):
+        return self.location
+
+class OrdType(models.Model):
+    type = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.type
+
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    datePlaced = models.DateField()
+    dateCompleted = models.DateField()
+    cubby = models.ForeignKey(Cubby, on_delete=models.CASCADE)
+    orderType = models.ForeignKey(OrdType, on_delete=models.CASCADE)
+    pickupDate = models.DateField()
+
+    def __str__(self):
+        return self.name
