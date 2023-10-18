9d886b0fcac9a348d44041461ac74e7460ecd6b5
judz5 <judz1105@gmail.com>
2023-10-17 20:25:46 -0500

    add individual order view. +button

--- PickupManager/Manager/urls.py
+++ PickupManager/Manager/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("newOrder", views.newOrder, name='newOrder'),
     path("viewOrders", views.viewOrders, name='viewOrders'),
+    path("order/<int:pk>/", views.order_detail_view, name='order_detail')
 ]
