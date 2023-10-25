9de8b0a9c0ab547a63ab0f86d0d21de056844a3f
judz5 <judz1105@gmail.com>
2023-10-25 10:13:36 -0500

    pickupdate not required added

--- PickupManager/Manager/forms.py
+++ PickupManager/Manager/forms.py
@@ -9,11 +9,13 @@ class NewOrderForm(ModelForm):
         fields = ['name', 'datePlaced', 'complete', 'cubby', 'orderType', 'pickupDate']
         widgets = {
             'datePlaced': forms.DateInput(attrs={'type': 'date'}),
-            'pickupDate': forms.DateInput(attrs={'type': 'date', 'required': False})
+            'pickupDate': forms.DateInput(attrs={'type': 'date'})
         }
 
     cubby = forms.ModelChoiceField(queryset=Cubby.objects.order_by('location'))
-    
+    def __init__(self, *args, **kwargs):
+        super(NewOrderForm, self).__init__(*args, **kwargs)
+        self.fields['pickupDate'].required = False
 
 class OrderSearchForm(forms.Form):
     name = forms.CharField(label="Name to Search", max_length = 100, required=False)
